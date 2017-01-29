9dee57a995753ec9040c69a9918b9987125a6ec9
Sam Vaughn <cornerkitten@users.noreply.github.com>
2017-01-28 20:51:27 -0500

    Move particles into ParticleContainer
     - Create cute stripe effect

--- src/go/behaviors/MomentEmitter.js
+++ src/go/behaviors/MomentEmitter.js
@@ -8,33 +8,44 @@ import { COMPONENT } from '../core/constants';
 const display_ = Symbol('display');
 const nextMomentPosition_ = Symbol('nextMomentPosition');
 const currentHour_ = Symbol('currentHour');
+const momentTexture_ = Symbol('momentTexture');
 
 export default class {
   constructor(params, services) {
     this[display_] = services.component(COMPONENT.DISPLAY);
-    this[currentHour_] = new Pixi.ParticleContainer();
+    this[currentHour_] = new Pixi.particles.ParticleContainer();
+    this[currentHour_].x = 128;
+    this[currentHour_].y = 512;
+    this[display_].addChild(this[currentHour_]);
+
     this[nextMomentPosition_] = {
-      x: 128,
-      y: 512,
+      x: 0,
+      y: 0,
     };
+
+    const moment = new Pixi.Graphics();
+    moment.beginFill(0xd6f5ff);
+    moment.drawRect(0, 0, 4, 4);
+    moment.endFill();
+    this[momentTexture_] = moment.generateTexture();
 
     setInterval(this.emit.bind(this), 250);
   }
 
+  // TODO Ideas
+  //  - Fade in particle as it falls
   emit() {
     const nextPos = this[nextMomentPosition_];
-    const moment = new Pixi.Graphics(this.x);
-    moment.x = nextPos.x;
-    moment.beginFill(0xd6f5ff);
-    moment.drawRect(0, 0, 8, 8);
-    moment.endFill();
-    this[display_].addChild(moment);
+    const momentSprite = new Pixi.Sprite(this[momentTexture_]);
+    momentSprite.x = nextPos.x;
+    momentSprite.y = -this[currentHour_].y;
+    this[currentHour_].addChild(momentSprite);
 
-    TweenLite.to(moment, 1.5, { y: nextPos.y });
+    TweenLite.to(momentSprite, 1.5, { y: nextPos.y });
 
     nextPos.x += 2;
-    if (nextPos.x >= 128 + 60) {
-      nextPos.x = 128;
+    if (nextPos.x >= 60) {
+      nextPos.x = 0;
       nextPos.y -= 8;
     }
   }
